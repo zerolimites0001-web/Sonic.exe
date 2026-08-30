33abafcab83d45ad9fe0ffd57427e60099061349
Zerolimites <Zerolimites0001@gmail.com>
2026-08-30 12:16:34 +0000

    fix: float_icon in OverlayService

--- app/src/main/java/com/sonicexe/OverlayService.kt
+++ app/src/main/java/com/sonicexe/OverlayService.kt
@@ -26,7 +26,7 @@ class OverlayService:Service(){
         mp=MediaPlayer.create(this,R.raw.scream)?.apply{setVolume(1f,1f);start()}
         try{(getSystemService(VIBRATOR_SERVICE) as Vibrator).vibrate(VibrationEffect.createWaveform(longArrayOf(0,300,100,500),-1))}catch(e:Exception){}
         wm=getSystemService(Context.WINDOW_SERVICE) as WindowManager
-        view=ImageView(this).apply{setImageResource(R.drawable.float); alpha=0.95f}
+        view=ImageView(this).apply{setImageResource(R.drawable.float_icon); alpha=0.95f}
         val p=WindowManager.LayoutParams(WindowManager.LayoutParams.WRAP_CONTENT,WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY, WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL, PixelFormat.TRANSLUCENT).apply{gravity=Gravity.CENTER}
         wm?.addView(view,p)
         view?.startAnimation(AnimationUtils.loadAnimation(this,android.R.anim.fade_in).apply{duration=200})
